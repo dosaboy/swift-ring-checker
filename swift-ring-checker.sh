303a8a26c47e0de397280df89b6bd5726558acc8
Edward Hope-Morley <edward.hope-morley@canonical.com>
2020-01-05 21:00:48 +0000

    updates

--- swift-ring-checker.sh
+++ swift-ring-checker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eu
 # Author: Edward Hope-Morley (opentastic@gmail.com)
 # Description: Swift Object Store Ring Checker Tool
-# Copyright (C) 2017-2018 Edward Hope-Morley
+# Copyright (C) 2017-2020 Edward Hope-Morley
 #
 # License:
 #
@@ -23,104 +23,122 @@ F_GRN="\033[32m"
 F_CYN="\033[36m"
 F_YLW="\033[33m"
 
-trap 'exit' SIGINT SIGKILL
+DEBUG=false
+RESOURCE=
+SERVER="\*"
+
+cleanup ()
+{
+    :
+}
+#trap cleanup INT KILL
 
 usage ()
 {
-    echo "USAGE: `basename $0` <resource>"
-    echo ""
-    echo "RESOURCE:"
-    echo "    [builders SERVER]|[rings SERVER]|hash|relations"
-    echo ""
-    echo "SERVER:"
-    echo "    account|container|object"
-    echo ""
-    echo ""
-    echo "ENV VARS:"
-    echo ""
-    echo "  JUJU_V1 - set this to use with Juju 1.x"
-    echo ""
-    echo ""
-}
+cat << EOF
+USAGE: swift-ring-checker OPTS RESOURCE
 
-(($#)) || { usage; exit 1; }
-for arg in $@; do if [ $arg = "-h" ]; then { usage; exit 1; }; fi; done
+OPTS:
+  -h|--help
+    Print this help message.
 
+RESOURCE:
+  builders SERVER|rings SERVER|hash|relations
 
-#!/bin/bash -eu
-get_units ()
-{
-cat <<- EOF| python - $1
-import json, subprocess, sys, os
-data = subprocess.check_output(['juju', 'status', '--format=json'])
-j = json.loads(data)
-if os.environ.get('JUJU_V1'):
-    applications = j['services']
-else:
-    applications = j['applications']
-
-proxies = []
-for key in applications:
-    charm = applications[key]['charm']
-    if 'swift-proxy' in charm:
-        units = applications[key]['units'].keys()
-        proxies+=units
-stores = []
-for key in applications:
-    charm = applications[key]['charm']
-    if 'swift-storage' in charm:
-        units = applications[key]['units'].keys()
-        stores+=units
-if len(sys.argv) > 1 and sys.argv[1] == 'proxy':
-    print '\n'.join(sorted(proxies))
-else:
-    print '\n'.join(sorted(stores))
+SERVER:
+  account|container|object
 
 EOF
 }
 
-read -a PROXY_UNITS<<<`get_units proxy`
-read -a STORAGE_UNITS<<<`get_units store`
+(($#)) || { usage; exit 1; }
+while (($#)); do
+    case $1 in
+        -h|--help)
+            usage
+            exit 0
+            ;;
+	--debug)
+            DEBUG=true
+            ;;
+        *)
+            RESOURCE="$1"
+	    if (($#>1)) && \
+		    ( [ "$RESOURCE" = "builders" ] || [ "$RESOURCE" = "rings" ] ); then
+		if [ "$2" = "account" ] || [ "$2" = "container" ] || [ "$2" = "object" ]; then
+                    SERVER="$2"
+                    shift
+	        fi
+            fi
+            ;;
+    esac
+    shift
+done
 
-for p_unit in ${PROXY_UNITS[@]}; do
-    echo -e "\n${F_GRN}== $p_unit${RES}"
-    if (($#>1)); then
-        builder="$2.builder"
-        ringgz="$2.ring.gz"
-    else
-        builder="\*.builder"
-        ringgz="\*.ring.gz"
-    fi
+$DEBUG && set -x
 
-    if [ "$1" = "rings" ]; then
-        # Display the ring md5sum in blue and display proxy service status
-        juju ssh $p_unit \
-                "echo -ne '${F_YLW}'; \
-                 sudo find /etc/swift/ -maxdepth 1 -name $ringgz| sort| xargs -l sudo md5sum; \
-                 echo -ne '${RES}'; \
-                 echo -n 'STATUS: '; \
-                 sudo systemctl status -n 0 swift-proxy --no-pager 2>/dev/null || sudo service swift-proxy status || true" 2>/dev/null
-    elif [ "$1" = "builders" ]; then
-        # Builder dev info
-        juju ssh $p_unit \
-                "sudo find /etc/swift/ -maxdepth 1 -name $builder| sort| xargs -l sudo swift-ring-builder; \
-                 echo -ne '${F_YLW}'; \
-                 sudo find /etc/swift/ -maxdepth 1 -name $builder| sort| xargs -l sudo md5sum; \
-                 echo -ne '${RES}'; \
-                 echo -n 'STATUS: '; \
-                 sudo systemctl status -n 0 swift-proxy --no-pager 2>/dev/null || sudo service swift-proxy status || true" 2>/dev/null
-    elif [ "$1" = "hash" ]; then
-        # Display proxy hash
-        juju ssh $p_unit 'sudo grep swift_hash /etc/swift/swift.conf' 2>/dev/null
-    elif [ "$1" = "relations" ]; then
+juju --version
+
+juju status
+
+get_units ()
+{
+    local app="$1"
+
+    # only return active units
+    juju status --format=json $app| jq -r ".applications.\"$app\".units | to_entries[] | select(.value.\"workload-status\".current==\"unknown\" | not) | .key"
+}
+
+readarray -t PROXY_UNITS<<<"`get_units swift-proxy`"
+declare -a STORAGE_UNITS=()
+readarray -t _S<<<"`get_units swift-storage-z1`"
+STORAGE_UNITS+=( ${_S[@]} )
+readarray -t _S<<<"`get_units swift-storage-z2`"
+STORAGE_UNITS+=( ${_S[@]} )
+readarray -t _S<<<"`get_units swift-storage-z3`"
+STORAGE_UNITS+=( ${_S[@]} )
+
+check_ring ()
+{
+    local unit="$1"
+    local ringgz="$2"
+
+    # Display the ring md5sum in blue and display proxy service status
+    juju ssh $unit \
+            "echo -ne '${F_YLW}'; \
+             sudo find /etc/swift/ -maxdepth 1 -name $ringgz| sort| xargs -l sudo md5sum; \
+             echo -ne '${RES}'; \
+             echo -n 'STATUS: '; \
+             sudo systemctl status -n 0 swift-proxy --no-pager" 2>/dev/null
+}
+
+check_builder ()
+{
+    local unit="$1"
+    local builder="$2"
+
+    # Builder dev info
+    juju ssh $unit \
+            "sudo find /etc/swift/ -maxdepth 1 -name $builder| sort| xargs -l sudo swift-ring-builder; \
+             echo -ne '${F_YLW}'; \
+             sudo find /etc/swift/ -maxdepth 1 -name $builder| sort| xargs -l sudo md5sum; \
+             echo -ne '${RES}'; \
+             echo -n 'STATUS: '; \
+             sudo systemctl status -n 0 swift-proxy --no-pager" 2>/dev/null
+}
+
+check_relations ()
+{
+        local p_unit="$1"
+
         # Display relation info
         for s_unit in ${STORAGE_UNITS[@]}; do
             echo -e "${F_YLW}= $s_unit${RES}"
 
             # storage from proxy
-            read -a rids<<<`juju run --unit $s_unit "relation-ids swift-storage"`
+            readarray -t rids<<<"`juju run -u $s_unit 'relation-ids swift-storage'`"
             for rid in ${rids[@]}; do
-                out=`juju run --unit $s_unit "relation-get -r $rid - $p_unit" 2>/dev/null` || true
+                out="`juju run -u $s_unit 'relation-get -r $rid - $p_unit' 2>/dev/null`" || true
                 if [ -n "$out" ]; then
                     echo -e "${F_CYN}= $s_unit <- $p_unit rid=$rid${RES}"
                     echo "$out"
@@ -129,7 +147,7 @@ for p_unit in ${PROXY_UNITS[@]}; do
 
             # storage from storage
             for rid in ${rids[@]}; do
-                out=`juju run --unit $s_unit "relation-get -r $rid - $s_unit" 2>/dev/null` || true
+                out="`juju run -u $s_unit 'relation-get -r $rid - $s_unit' 2>/dev/null`" || true
                 if [ -n "$out" ]; then
                     echo -e "${F_CYN}= $s_unit <- $s_unit rid=$rid${RES}"
                     echo "$out"
@@ -137,9 +155,9 @@ for p_unit in ${PROXY_UNITS[@]}; do
             done
 
             # proxy from storage
-            read -a rids<<<`juju run --unit $p_unit "relation-ids swift-storage"`
+            readarray -t rids<<<"`juju run -u $p_unit 'relation-ids swift-storage'`"
             for rid in ${rids[@]}; do
-                out=`juju run --unit $p_unit "relation-get -r $rid - $s_unit" 2>/dev/null` || true
+                out="`juju run -u $p_unit 'relation-get -r $rid - $s_unit' 2>/dev/null`" || true
                 if [ -n "$out" ]; then
                     echo -e "${F_CYN}= $p_unit <- $s_unit rid=$rid${RES}"
                     echo "$out"
@@ -148,35 +166,48 @@ for p_unit in ${PROXY_UNITS[@]}; do
         done
 
         # proxy from proxy
-        read -a rids<<<`juju run --unit $p_unit "relation-ids cluster"`
+        readarray -t rids<<<"`juju run -u $p_unit 'relation-ids cluster'`"
         for _p_unit in ${PROXY_UNITS[@]}; do
             for rid in ${rids[@]}; do
-                out=`juju run --unit $p_unit "relation-get -r $rid - $_p_unit" 2>/dev/null` || true
+                out="`juju run -u $p_unit 'relation-get -r $rid - $_p_unit' 2>/dev/null`" || true
                 if [ -n "$out" ]; then
                     echo -e "${F_CYN}= $p_unit <- $_p_unit rid=$rid${RES}"
                     echo "$out"
                 fi
             done
         done
+}
+
+for p_unit in ${PROXY_UNITS[@]}; do
+    echo -e "\n${F_GRN}== $p_unit${RES}"
+    builder="${SERVER}.builder"
+    ringgz="${SERVER}.ring.gz"
+
+    if [ "$RESOURCE" = "rings" ]; then
+        check_ring $p_unit $ringgz
+    elif [ "$RESOURCE" = "builders" ]; then
+        check_builder $p_unit $builder
+    elif [ "$RESOURCE" = "hash" ]; then
+        # Display proxy hash
+        juju ssh $p_unit -- sudo grep swift_hash /etc/swift/swift.conf 2>/dev/null
+    elif [ "$RESOURCE" = "relations" ]; then
+        check_relations $p_unit
     else
-        echo "ERROR: unknown action '$1'"
+        echo "ERROR: unknown resource '$RESOURCE'"
         usage
         exit 1
     fi
 done
 
-for s_unit in ${STORAGE_UNITS[@]}; do
-    if (($#>1)); then
-        ringgz="$2.ring.gz"
-    else
-        ringgz="\*.ring.gz"
-    fi
-
-    if [ "$1" = "rings" ]; then
+if [ "$RESOURCE" = "rings" ]; then
+    for s_unit in ${STORAGE_UNITS[@]}; do
+        ringgz="${SERVER}.ring.gz"
         echo -e "\n${F_GRN}== $s_unit${RES}"
         echo -ne "${F_YLW}"
-        juju ssh $s_unit \
-            "sudo find /etc/swift/ -maxdepth 1 -name $ringgz| sort| xargs -l sudo md5sum" 2>/dev/null
+        juju ssh $s_unit -- \
+            "sudo find /etc/swift/ -maxdepth 1 -name $ringgz| sort| xargs -l sudo md5sum 2>/dev/null"
         echo -ne "${RES}"
-    fi
-done
+    done
+fi
+
+echo "Done."
